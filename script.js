3c12c7d63c782c19a21cd41e5739cd3ebfdbb695
IT20615734 <it20615734@my.sliit.lk>
2023-07-18 23:21:29 +0530

    sticky nav bar

--- script.js
+++ script.js
@@ -9,13 +9,19 @@ window,onscroll=() => {
         let height =sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && < offset+height) {
+        if(top >= offset && top < offset + height) {
 
             navLinks.forEach(links =>{
                 links.classList.remove('active');
-                document.querySelector("header nav a");
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+
 
             })
         }
     })
+
+    // sticy nav bar
+    let header=document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY>100);
 }
